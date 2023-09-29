b8ca42d840f115a4c703737dded8d6e168ba4c50
Orlando Saraiva Jr <orlandosaraivajr@gmail.com>
2023-09-28 22:05:49 -0300

    Projeto "É Feriado" com ORM

--- django/feriados/core/models.py
+++ django/feriados/core/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class FeriadoModel(models.Model):
+    nome = models.CharField('Feriado',max_length=50)
+    dia = models.IntegerField('Data')
+    mes = models.IntegerField('Mês')
+    modificado_em = models.DateTimeField(verbose_name='modificado em', auto_now_add=False, auto_now=True)
+
+    def __str__(self):
+        return self.nome
--- django/feriados/core/tests.py
+++ django/feriados/core/tests.py
@@ -21,3 +21,26 @@ class IndependenciaTest(TestCase):
     def test_texto(self):
         self.assertContains(self.resp, 'Independência')
         self.assertContains(self.resp, 'morte')
+
+
+from core.models import FeriadoModel
+from datetime import datetime
+
+class FeriadoModelTest(TestCase):
+    def setUp(self):
+        self.feriado = 'Natal'
+        self.mes = 12
+        self.dia = 25
+        self.cadastro = FeriadoModel(nome=self.feriado, dia=self.dia, mes=self.mes)
+        self.cadastro.save()
+    
+    def test_created(self):
+        self.assertTrue(FeriadoModel.objects.exists())
+
+    def test_modificado_em(self):
+        self.assertIsInstance(self.cadastro.modificado_em, datetime)
+
+    def test_nome_feriado(self):
+        nome = self.cadastro.__dict__.get('nome', '')
+        self.assertEqual(nome, self.feriado)
+    
